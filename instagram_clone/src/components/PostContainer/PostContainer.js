fcfb042fc0e5ca131ca435ee3200c92a210b5076
Dil Sombayeva <dilyasom@gmail.com>
2019-05-22 08:39:00 -0700

    style PostConainer

--- instagram_clone/src/components/PostContainer/PostContainer.js
+++ instagram_clone/src/components/PostContainer/PostContainer.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
+import './PostContainer.css';
 
 const PostContainer = props => {
  console.log('props in post container: ', props);
  return (
-  <div>
-   <img src={props.post.imageUrl} alt="image" />
+  <div className="card_wrapper">
+   <b>
+    <p className="textPad float-left userHeader">
+     <img
+      src={props.post.thumbnailUrl}
+      alt="image thumbnail"
+      className="thumb"
+     />{' '}
+     {props.post.username}
+    </p>
+   </b>
+   <div>
+    <img className="img-fluid" src={props.post.imageUrl} alt="image" />
+   </div>
    {props.post.comments.map(comment => {
     return <CommentSection key={Math.random(Date.now())} comment={comment} />;
    })}
+   <p>
+    <input className="inputField" placeholder="Add a comment..." />
+   </p>
   </div>
  );
 };
