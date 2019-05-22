e5a1a034d438b58a185a486f325c4abcbbba188a
Dil Sombayeva <dilyasom@gmail.com>
2019-05-21 20:54:30 -0700

    refactor serach bar

--- instagram_clone/src/components/SearchBar/SearchBar.js
+++ instagram_clone/src/components/SearchBar/SearchBar.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const SearchBar = () => {
  return (
-  <div>
-   <h1>Instagram</h1>
-   <input placeholder="Search" />
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+   <a class="navbar-brand mb-0 h1">Instagram</a>
+   <form class="form-inline">
+    <input
+     class="form-control mr-sm-2"
+     type="search"
+     placeholder="Search"
+     aria-label="Search"
+    />
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+     Search
+    </button>
+   </form>
+  </nav>
  );
 };
 
