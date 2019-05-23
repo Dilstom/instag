1679eb17b376b0b35a673802b764421fba1d6432
Dil Sombayeva <dilyasom@gmail.com>
2019-05-22 18:52:11 -0700

    add propTypes to CommentSection

--- instagram_clone/src/components/CommentSection/CommentSection.js
+++ instagram_clone/src/components/CommentSection/CommentSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './CommentSection.css';
+import PropTypes from 'prop-types';
 
 const CommentSection = props => {
  console.log('props in comments: ', props);
@@ -8,6 +9,13 @@ const CommentSection = props => {
    <b>{props.comment.username}</b> {props.comment.text}
   </div>
  );
+};
+
+CommentSection.propTypes = {
+ comment: PropTypes.shape({
+  text: PropTypes.string,
+  username: PropTypes.string,
+ }),
 };
 
 export default CommentSection;
