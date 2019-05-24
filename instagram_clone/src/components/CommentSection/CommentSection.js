7013c5cb1cf4c820e58098b371495af91f7b726b
Dil Sombayeva <dilyasom@gmail.com>
2019-05-24 16:34:19 -0700

    refactor CommentSection

--- instagram_clone/src/components/CommentSection/CommentSection.js
+++ instagram_clone/src/components/CommentSection/CommentSection.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './CommentSection.css';
 import PropTypes from 'prop-types';
+import Comment from './Comment';
+import CommentInput from './CommentInput';
 
 const CommentSection = props => {
  console.log('props in comments: ', props);
  return (
-  <div className="textPad eachComment">
-   <b>{props.comment.username}</b> {props.comment.text}
+  <div>
+   <div className="textPad eachComment">
+    {props.comments.map(comment => {
+     return <Comment key={Math.random()} comment={comment} />;
+    })}
+   </div>
+   <CommentInput />
   </div>
  );
 };
 
-CommentSection.propTypes = {
- comment: PropTypes.shape({
-  text: PropTypes.string,
-  username: PropTypes.string,
- }),
-};
+// CommentSection.propTypes = {
+//  comment: PropTypes.shape({
+//   text: PropTypes.string,
+//   username: PropTypes.string,
+//  }),
+// };
 
 export default CommentSection;
