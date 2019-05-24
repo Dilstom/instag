d34a52346c1c3d3befc52cba04146352d1eb39cf
Dil Sombayeva <dilyasom@gmail.com>
2019-05-24 16:35:01 -0700

    refactor PostContainer.js

--- instagram_clone/src/components/PostContainer/PostContainer.js
+++ instagram_clone/src/components/PostContainer/PostContainer.js
@@ -2,36 +2,25 @@ import React from 'react';
 import CommentSection from '../CommentSection/CommentSection';
 import './PostContainer.css';
 import PropTypes from 'prop-types';
+import Post from './Post';
 
 const PostContainer = props => {
- console.log('props in post container: ', props);
+ //  console.log('props', props);
  return (
-  <div className="card_wrapper">
-   <b>
-    <p className="textPad float-left userHeader">
-     <img src={props.post.thumbnailUrl} alt="thumbnail" className="thumb" />{' '}
-     {props.post.username}
-    </p>
-   </b>
-   <div>
-    <img className="img-fluid" src={props.post.imageUrl} alt="Post" />
-   </div>
-   {props.post.comments.map(comment => {
-    return <CommentSection key={Math.random(Date.now())} comment={comment} />;
-   })}
-   <p>
-    <input className="inputField" placeholder="Add a comment..." />
-   </p>
+  <div>
+   {props.posts.map(post => (
+    <Post key={post.imageUrl} post={post} />
+   ))}
   </div>
  );
 };
 
-PostContainer.propTypes = {
- post: PropTypes.shape({
-  img: PropTypes.string,
-  username: PropTypes.string,
-  comments: PropTypes.array,
-  thumbnailUrl: PropTypes.string,
- }),
-};
+// PostContainer.propTypes = {
+//  post: PropTypes.shape({
+//   img: PropTypes.string,
+//   username: PropTypes.string,
+//   comments: PropTypes.array,
+//   thumbnailUrl: PropTypes.string,
+//  }),
+// };
 export default PostContainer;
