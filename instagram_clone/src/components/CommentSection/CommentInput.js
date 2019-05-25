a60e9f9b2b27b7c5cc4eb31cc4fa6959baf1bae4
Dil Sombayeva <dilyasom@gmail.com>
2019-05-24 18:05:22 -0700

    fixed a small bug

--- instagram_clone/src/components/CommentSection/CommentInput.js
+++ instagram_clone/src/components/CommentSection/CommentInput.js
@@ -4,12 +4,12 @@ import './CommentSection.css';
 const CommentInput = props => {
  return (
   <div>
-   <form onSubmit={props.onclickProps}>
+   <form onSubmit={props.submitHandler}>
     <input
-     className="inputField"
+     type="text"
      value={props.comment}
-     name="comment"
-     onChange={props.onChangeProps}
+     className="inputField"
+     onChange={props.changeHandler}
      placeholder="Add a comment... "
     />
    </form>
