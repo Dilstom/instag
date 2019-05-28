ea0bfc77042ebf81c0809684bf33d94d303276b1
Dil Sombayeva <dilyasom@gmail.com>
2019-05-27 17:46:29 -0700

    remove class

--- instagram_clone/src/components/CommentSection/Comment.js
+++ instagram_clone/src/components/CommentSection/Comment.js
@@ -4,7 +4,7 @@ import './CommentSection.css';
 const Comment = props => {
  console.log('props in comment: ', props);
  return (
-  <div className="textPad eachComment">
+  <div className=" eachComment">
    <b>{props.comment.username}</b> {props.comment.text}
   </div>
  );
