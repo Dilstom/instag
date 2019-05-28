8d43274ec87ba8dce53d960ab590d332cc7dfe1a
Dil Sombayeva <dilyasom@gmail.com>
2019-05-27 19:05:22 -0700

    comment out console.log

--- instagram_clone/src/components/CommentSection/Comment.js
+++ instagram_clone/src/components/CommentSection/Comment.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CommentSection.css';
 
 const Comment = props => {
- console.log('props in comment: ', props);
+ //  console.log('props in comment: ', props);
  return (
   <div className=" eachComment">
    <b>{props.comment.username}</b> {props.comment.text}
